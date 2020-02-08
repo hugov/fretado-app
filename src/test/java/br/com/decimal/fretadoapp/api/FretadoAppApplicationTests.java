e29c688b942b82b2e7b9019c183e8a881ded423d
Vitor Hugo <hugov1983@gmail.com>
2020-02-07 21:20:37 -0300

    Inclusão inicial do projeto

---
+++ src/test/java/br/com/decimal/fretadoapp/api/FretadoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.decimal.fretadoapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FretadoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
