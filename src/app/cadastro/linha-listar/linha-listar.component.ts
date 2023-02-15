d741cd00268d670236ff58e2ca217a1d452c455c
Vitor Hugo Oliveira <hugov1983@gmail.com>
2023-02-15 10:36:50 -0300

    Corrigido a listagem de linha em memória

--- src/app/cadastro/linha-listar/linha-listar.component.ts
+++ src/app/cadastro/linha-listar/linha-listar.component.ts
@@ -4,6 +4,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { Linha } from '../model/linha.model';
 import { LinhaService } from '../service/linha.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-linha-listar',
@@ -19,9 +20,9 @@ export class LinhaListarComponent implements OnInit {
     situacao: 0,
   }
 
-  linhas!: Linha[];
+  //linhas: Linha[]
+  linhas : Linha[] = [];
 
-  //itens: Linha[] = [];
   dataSource = new MatTableDataSource<Linha>(this.linhas);
   displayedColumns: string[] = ['id', 'codigoLinha', 'descricao', 'situacao'];
 
@@ -34,26 +35,35 @@ export class LinhaListarComponent implements OnInit {
 
   ngOnInit() : void {
     //this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    //this.dataSource.sort = this.sort;
     this.getLista()
+    this.dataSource = new MatTableDataSource<Linha>(this.linhas);
+
+    console.log('Linha Component - ngOnInit() - Quantidade de registros:' + this.linhas)
   }
 
+  /*
   ngAfterViewInit() {
     //this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
+  */
 
   addLineItem() {
+
+    //if(this.linhas == undefined)
+    //  this.linhas = []
+
     this.linhas.push(this.linha);
     this.dataSource = new MatTableDataSource<Linha>(this.linhas);
+    console.log('Linha Component - addLineItem: ', this.linhas)
   }
 
   private getLista() {
-    this.linhaService.getLinhas().subscribe(p => this.linhas = p);
+    console.log('Linha Component - getLista()')
+    this.linhaService.getLinhas().subscribe(linhas => this.linhas = linhas);
 
-    console.log('Linhas:', this.linhas)
-
-    this.dataSource = new MatTableDataSource<Linha>(this.linhas);
+    //this.dataSource = new MatTableDataSource<Linha>(this.linhas);
   }
 
 }
--- src/app/cadastro/service/linha.service.ts
+++ src/app/cadastro/service/linha.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Linha } from '../model/linha.model';
 
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
@@ -11,18 +11,30 @@ import { catchError, retry } from 'rxjs/operators';
 export class LinhaService {
 
   private linhaUrl = 'api/linhas/';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'No-Auth': 'True',
+    }),
+  };
   
   constructor(private http: HttpClient) { 
     console.log('Carregando o serviço de linha')
   }
 
   getLinhas(): Observable<Linha[]> {
-    return this.http.get<Linha[]>(this.linhaUrl).pipe(
-      retry(2),
-      catchError((error: HttpErrorResponse) => {
-        console.error(error);
-        return throwError(error);
-      })
-    );
+
+    let reqHeader = new HttpHeaders({ 'Content-Type': 'application/json', 'No-Auth': 'True' });
+    /*
+    return this.http.get(this.linhaUrl, { headers: reqHeader })
+    .subscribe((resp: any) => {
+      console.log(resp);
+    }, err => {
+      console.log(err);
+    });
+    */
+
+    return this.http.get<Linha[]>(this.linhaUrl).pipe();
   }
 }
